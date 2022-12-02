bec0edf659719e56bea2cfcb12d11f1a50dbd8cc
Camix777 <c.linares1@uniandes.edu.co>
2022-12-01 20:14:17 -0500

    sapo

--- src/ReporteJugador.java
+++ src/ReporteJugador.java
@@ -1,5 +1,6 @@
+import java.io.Serializable;
 
-public class ReporteJugador {
+public class ReporteJugador implements Serializable{
 
     private String nombrePartido;
     private int minutosJugados;
@@ -14,10 +15,13 @@ public class ReporteJugador {
     private int penaltisErrados;
     private int amarillas;
     private int rojas;
+    private int tiros_libres;
+    private int manos;
+    private int goles_de_tiro_libre;
 
     public ReporteJugador(String partidoBus, int minJugados, int minIngresado, int minSalido, int goles,
             int golesPenaltis, int autogoles, int asistencias, int golesRecibidos, int penaltisDetenidos,
-            int penaltisErrados, int tarjetasAmarillas, int tarjetasRojas) {
+            int penaltisErrados, int tarjetasAmarillas, int tarjetasRojas, int tiros_libres, int manos, int goles_de_tiro_libre) {
 
         this.nombrePartido = partidoBus;
         this.minutosJugados = minJugados;
@@ -32,7 +36,9 @@ public class ReporteJugador {
         this.penaltisErrados = penaltisErrados;
         this.amarillas = tarjetasAmarillas;
         this.rojas = tarjetasRojas;
-
+        this.tiros_libres=tiros_libres;
+        this.manos=manos;
+        this.goles_de_tiro_libre=goles_de_tiro_libre;
     }
 /**
  * Retorna el nombre del partido del cual es el reporte
@@ -84,5 +90,14 @@ public int getTarjetasAmarillas(){
 }
 public int getTarjetasRojas(){
     return rojas;
+}
+public int getTirosLibres(){
+    return tiros_libres;
+}
+public int getManos(){
+    return manos;
+}
+public int getGolesdeTiroLibre(){
+    return goles_de_tiro_libre;
 }
 }
